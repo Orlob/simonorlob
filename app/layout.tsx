25d91515c7a6c7de939b240a30e8f65e9650ef8a
Simon <simon.orlob@gmail.com>
2025-02-24 10:11:05 +0100

    add datafast

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { getSEOTags } from "@/libs/seo";
 import ClientLayout from "@/components/LayoutClient";
 import config from "@/config";
 import "./globals.css";
+import Script from "next/script";
 
 const font = Inter({ subsets: ["latin"] });
 
@@ -26,6 +27,15 @@ export default function RootLayout({ children }: { children: ReactNode }) {
 			data-theme={config.colors.theme}
 			className={font.className}
 		>
+			<head>
+				<Script
+					defer
+					data-website-id="67bc375e55caf77fa23eb4a1"
+					data-domain="simonorlob.de"
+					src="https://datafa.st/js/script.js"
+					strategy="afterInteractive"
+				/>
+			</head>
 			<body>
 				{/* ClientLayout contains all the client wrappers (Crisp chat support, toast messages, tooltips, etc.) */}
 				<ClientLayout>{children}</ClientLayout>
